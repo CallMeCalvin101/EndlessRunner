9e76ee18e3daa34aa0b23793f1716f78d272f702
Vincent Kurniadjaja <calvinkurniadjaja@gmail.com>
2022-04-21 15:47:06 -0700

    made a group for buttons

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -20,26 +20,18 @@ class Play extends Phaser.Scene {
         this.add.rectangle(0, 2 * game.config.height / 3, game.config.width, game.config.height / 3, 0xFF7254).setOrigin(0, 0);
 
         // Test Buttons
-        this.testButton1 = new Button(this, game.config.width/2 - 60, game.config.height * (5/6) - 36, 'button').setOrigin(0);
-        this.testButton2 = new Button(this, game.config.width/2, game.config.height * (5/6) - 36, 'button').setOrigin(0);
-        this.testButton3 = new Button(this, game.config.width/2 + 60, game.config.height * (5/6) - 36, 'button').setOrigin(0);
-        this.testButton4 = new Button(this, game.config.width/2 - 60, game.config.height * (5/6), 'button').setOrigin(0);
-        this.testButton5 = new Button(this, game.config.width/2, game.config.height * (5/6), 'button').setOrigin(0);
-        this.testButton6 = new Button(this, game.config.width/2 + 60, game.config.height * (5/6), 'button').setOrigin(0);
-        this.testButton7 = new Button(this, game.config.width/2 - 60, game.config.height * (5/6) + 36, 'button').setOrigin(0);
-        this.testButton8 = new Button(this, game.config.width/2, game.config.height * (5/6) + 36, 'button').setOrigin(0);
-        this.testButton9 = new Button(this, game.config.width/2 + 60, game.config.height * (5/6) + 36, 'button').setOrigin(0);
+        this.testButtons = this.add.group();
+        for (let i = 0; i < 5; i++) {
+            for (let j = 0; j < 5; j++) {
+                let testButton = new Button(this, 90 + (60 * i), game.config.height * (2/3) + 20 + (30 * j), 'button').setOrigin(0, 0);
+                this.testButtons.add(testButton);
+            }
+        }
+
+        this.testButtons.runChildUpdate = true;
     }
 
     update() {
-        this.testButton1.update();
-        this.testButton2.update();
-        this.testButton3.update();
-        this.testButton4.update();
-        this.testButton5.update();
-        this.testButton6.update();
-        this.testButton7.update();
-        this.testButton8.update();
-        this.testButton9.update();
+        // no functions yet
     }
 }
