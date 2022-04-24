1c8de84c7e4848da3bbd9fcec853ebac7c5426cb
dyang46 <dyang46@ucsc.edu>
2022-04-24 06:07:16 -0700

    hope this is the right one D

---
+++ src/scenes/Hp.js
@@ -0,0 +1,39 @@
+class Hp {
+    constructor(scene, x, y) {
+        this.hbar = new Phaser.GameObjects.Graphics(scene);  
+        this.x = x;
+        this.y = y;
+        this.value = 100;
+        this.pval = 0.8;
+        this.draw();
+        this.healthbar=scene.add.existing(this.hbar);
+
+    }  
+ 
+    decrease (atkhp){
+        this.value -= atkhp;
+        if(this.value <0){
+            this.value = 0;
+        }
+        this.draw();
+        return (this.value ==0);
+    }
+
+    increase(addhp){
+        this.value -= addhp;
+        if(this.value >= 100){
+            this.value = 100;
+        }
+        this.draw();
+        return (this.value ==100);
+    }
+    draw(){
+        this.hbar.clear();
+        this.hbar.fillStyle(0x000000);
+        this.hbar.fillRect(this.x, this.y, 200,10);
+        this.hbar.fillStyle(0xffffff);
+        this.hbar.fillRect(this.x+2, this.y+2, 180, 10);
+        var d = Math.floor(this.pval* this.value);
+        this.hbar.fillRect(this.x+2, this.y+2,d,10);
+    }
+}
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -6,12 +6,17 @@ class Play extends Phaser.Scene {
     preload() {
         //place back ground 
         this.load.image('magicworld', './assets/magicworld.png');
+        this.load.image ('arrow','./assets/canon.png');
+        this.load.image ('still','./assets/camper.png');
+        this.load.image('blocker','./assets/bar.png');
         // Test Assets
         this.load.spritesheet('button', './assets/button.png', {frameWidth: 48, frameHeight: 24, startFrame: 0, endFrame: 1});
         this.load.spritesheet('miku', './assets/miku.png', {frameWidth: 50, frameHeight: 50, startFrame: 0, endFrame: 3});
     }
 
     create() {
+        // a vector to track player's position
+        this.ptr = new Phaser.Math.Vector2();
         //variables/settings for physics engine
         this.ACCELERATION = 2000;
         this.MAX_X_VEL = 500; 
@@ -29,10 +34,11 @@ class Play extends Phaser.Scene {
         this.magicworld = this.add.image(200,220,'magicworld');
         // Variable for cursor
         gamePointer = this.input.activePointer;
-
+        
         // Test UI
         this.add.rectangle(0, 2 * game.config.height / 3, game.config.width, game.config.height / 3, 0xFF7254).setOrigin(0, 0);
-
+        
+        
         // Test Buttons
         this.testButton1 = new Button(this, game.config.width/2 - 60, game.config.height * (5/6) - 36, 'button').setOrigin(0);
         this.testButton2 = new Button(this, game.config.width/2, game.config.height * (5/6) - 36, 'button').setOrigin(0);
@@ -43,17 +49,50 @@ class Play extends Phaser.Scene {
         this.testButton7 = new Button(this, game.config.width/2 - 60, game.config.height * (5/6) + 36, 'button').setOrigin(0);
         this.testButton8 = new Button(this, game.config.width/2, game.config.height * (5/6) + 36, 'button').setOrigin(0);
         this.testButton9 = new Button(this, game.config.width/2 + 60, game.config.height * (5/6) + 36, 'button').setOrigin(0);
+        //create player animate
         this.anims.create({
             key: 'a1',            
             frames: this.anims.generateFrameNumbers('miku', {start: 0, end: 3, first: 0}),
             frameRate: 4,
             repeat: -1
         });
-        this.bar = new player(this, 100, 100,'miku',0);
-        this.bar.play('a1');
-    }
 
+        // player
+        this.blocker= this.physics.add.image(game.config.width/2,2*game.config.height/3,'blocker');
+        this.blocker.setImmovable(true);
+        this.player = this.physics.add.sprite(100,100,'miku'); 
+        this.player.setCollideWorldBounds(true);  
+        this.physics.add.collider(this.player, this.blocker);
+        // move player to the clicked/tapped position
+        this.input.on('pointerdown', function (gamePointer)
+        {
+            //this.arrow.setPosition(gamePointer.x, gamePointer.y);
+            this.ptr.x=gamePointer.x;
+            this.ptr.y=gamePointer.y;
+            this.player.play('a1');
+            this.physics.moveToObject(this.player, gamePointer, 400);  //player speed, can always change         
+            //////////////////////////////////////////////////////
+            //I need to add more logic here to keep the player stay in the top half
+        },this);
+        // Add a new hp bar deatures: Hp.increase(var) Hp.decrease(var);
+        this.hp= new Hp(this,game.config.width / 3, game.config.height/2+120);
+    }
     update() {
+        //////////////////////// PLAYER MOVEMENT ///////////////////////////
+        // calculate the distance between player and the clicked/tapped spot
+        // line xx~ xx from https://phaser.io/examples/v2/input/follow-mouse
+        var distance = Phaser.Math.Distance.Between(this.player.x, this.player.y, this.ptr.x, this.ptr.y);
+
+        if (this.player.body.speed > 0)
+        {
+        
+            if (distance < 4)
+            {
+            this.player.body.reset(this.ptr.x, this.ptr.y);
+            }
+        }
+        // end of borrowing code
+        //////////////////////// PLAYER MOVEMENT ///////////////////////////
         if(this.obstacle01.body.blocked.left)       //if obstacle hits left side of screen, reset it, play standard animation (instead of being broken animation if player has collided with obstacle)
         {
             // console.log("blocked on left") //for debugging
@@ -96,8 +135,7 @@ class Play extends Phaser.Scene {
             this.character.body.setDragX(this.DRAG);
             this.character.body.setDragY(this.DRAG);
         }
-
-       
-
+ 
     }
+   
 }
